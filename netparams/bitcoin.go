df55428e7be95c7397faab708abb43a8dd860c75
Runchao Han <runchao.han@monash.edu>
2022-08-10 10:53:29 +1000

    copyright

--- netparams/bitcoin.go
+++ netparams/bitcoin.go
@@ -1,3 +1,4 @@
+// Copyright (c) 2022-2022 The Babylon developers
 // Copyright (c) 2013-2015 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
