0ce568d40a8912d54e2d5aba434a1f7de8eb6ab8
Runchao Han <runchao.han@monash.edu>
2022-08-17 15:07:16 +1000

    comments

--- btcclient/utils.go
+++ btcclient/utils.go
@@ -6,6 +6,10 @@ import (
 	"github.com/btcsuite/btcutil"
 )
 
+// IndexedBlock is a BTC block with some extra information compared to wire.MsgBlock, including:
+// - block height
+// - txHash, txHashWitness, txIndex for each Tx
+// These are necessary for generating Merkle proof (and thus the `MsgInsertBTCSpvProof` message in babylon) of a certain tx
 type IndexedBlock struct {
 	Height int32
 	Header *wire.BlockHeader
@@ -31,3 +35,5 @@ func (ib *IndexedBlock) MsgBlock() *wire.MsgBlock {
 func (ib *IndexedBlock) BlockHash() chainhash.Hash {
 	return ib.Header.BlockHash()
 }
+
+// TODO: function for generating Merkle proof of a certain tx
