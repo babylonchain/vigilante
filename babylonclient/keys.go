8a66ed0cc2fec772d7bf2b2a0ea5f450c06999cb
Runchao Han <elvisage941102@gmail.com>
2022-08-29 10:19:33 +1000

    init

--- babylonclient/keys.go
+++ babylonclient/keys.go
@@ -1,9 +1,19 @@
 package babylonclient
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (c *Client) GetAddr() (sdk.AccAddress, error) {
 	return c.ChainClient.GetKeyAddress()
+}
+
+func (c *Client) MustGetAddr() sdk.AccAddress {
+	addr, err := c.ChainClient.GetKeyAddress()
+	if err != nil {
+		panic(fmt.Errorf("Failed to get signer: %v", err))
+	}
+	return addr
 }
--- reporter/btc_handlers.go
+++ reporter/btc_handlers.go
@@ -2,6 +2,9 @@ package reporter
 
 import (
 	"github.com/babylonchain/vigilante/types"
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/wire"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (r *Reporter) indexedBlockHandler() {
@@ -11,9 +14,31 @@ func (r *Reporter) indexedBlockHandler() {
 	for {
 		select {
 		case ib := <-r.btcClient.IndexedBlockChan:
+			header := ib.Header
+			signer := r.babylonClient.MustGetAddr()
 			log.Infof("Start handling block %v from BTC client", ib.BlockHash())
-			// dispatch the indexed block to the handler
-			r.handleIndexedBlock(ib)
+
+			// wrap header into MsgInsertHeader message and submit to Babylon
+			if err := r.handleHeader(header, signer); err != nil {
+				log.Errorf("Failed to handle header %v from Bitcoin: %v", header.BlockHash(), err)
+				// TODO: handle error
+			}
+			log.Infof("Successfully submitted MsgInsertHeader with header hash %v to Babylon", header.BlockHash())
+			// TODO: ensure that the header is inserted into BTCLightclient, then filter txs
+			// (see relevant discussion in https://github.com/babylonchain/vigilante/pull/5)
+
+			// for each tx,
+			// - try to extract a ckpt half from it
+			// - cache the half locally
+			// - try to match the half with an existing half
+			// - wrap the two halves to InsertBTCSpvProof and submit to Babylon
+			for _, tx := range ib.Txs {
+				if err := r.handleTx(tx); err != nil {
+					log.Errorf("Failed to handle Tx %v from Bitcoin: %v", tx.Hash(), err)
+					// TODO: handle error
+					continue
+				}
+			}
 		case <-quit:
 			// We have been asked to stop
 			return
@@ -21,40 +46,19 @@ func (r *Reporter) indexedBlockHandler() {
 	}
 }
 
-func (r *Reporter) handleIndexedBlock(ib *types.IndexedBlock) {
-	// handle BTC header
-	// - forward this header to BTCLightclient module
-	header := ib.Header
-	log.Debugf("Received a new block %v", header.BlockHash())
-	signer, err := r.babylonClient.GetAddr()
-	if err != nil {
-		log.Errorf("Failed to get signer: %v", err)
-		return
-	}
-
+func (r *Reporter) handleHeader(header *wire.BlockHeader, signer sdk.AccAddress) error {
 	msgInsertHeader := types.NewMsgInsertHeader(r.babylonClient.Cfg.AccountPrefix, signer, header)
 	log.Debugf("signer: %v, headerHex: %v", signer, msgInsertHeader.Header.MarshalHex())
-	res, err := r.babylonClient.InsertHeader(msgInsertHeader)
+	_, err := r.babylonClient.InsertHeader(msgInsertHeader)
 	if err != nil {
-		log.Errorf("Failed to submit MsgInsertHeader with header hash %v to Babylon: %v", msgInsertHeader.Header.Hash(), err)
-		return
+		return err
 	}
-	log.Infof("Successfully submitted MsgInsertHeader with header hash %v to Babylon with response %v", msgInsertHeader.Header.Hash(), res)
+	return nil
+}
 
-	// handle each tx
-	// TODO: ensure that the header is inserted into BTCLightclient, then filter txs
-	// (see relevant discussion in https://github.com/babylonchain/vigilante/pull/5)
-	for _, tx := range ib.Txs {
-		// TODO: decode to objects
-		entry1, entry2 := filterTx(tx.MsgTx())
-		if entry1 != nil {
-			log.Infof("Found a first half %v from Tx %v", entry1, tx.Hash())
-		} else if entry2 != nil {
-			log.Infof("Found a second half %v from Tx %v", entry1, tx.Hash())
-		} else {
-			continue
-		}
-		// TODO: check if the filtered entry can assemble with an existing entry to a new valid ckpt
-		// TODO: upon a newly assembled checkpoint, forward it to BTCCheckpoint module
-	}
+func (r *Reporter) handleTx(*btcutil.Tx) error {
+	// TODO: decode to objects
+	// TODO: check if the filtered entry can assemble with an existing entry to a new valid ckpt
+	// TODO: upon a newly assembled checkpoint, forward it to BTCCheckpoint module
+	return nil
 }
