c46e0ae06746f872b8f6ac4c93e948f5bb7fdd17
Runchao Han <elvisage941102@gmail.com>
2022-09-05 15:16:09 +1000

    retrieve metadata upon bootstrapping

--- babylonclient/query.go
+++ babylonclient/query.go
@@ -1,27 +1,29 @@
 package babylonclient
 
 import (
+	btclightclienttypes "github.com/babylonchain/babylon/x/btclightclient/types"
 	epochingtypes "github.com/babylonchain/babylon/x/epoching/types"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/strangelove-ventures/lens/client/query"
 )
 
 // QueryStakingParams queries staking module's parameters via ChainClient
 // code is adapted from https://github.com/strangelove-ventures/lens/blob/v0.5.1/cmd/staking.go#L128-L149
-func (c *Client) QueryStakingParams() (stakingtypes.Params, error) {
+func (c *Client) QueryStakingParams() (*stakingtypes.Params, error) {
 	query := query.Query{Client: c.ChainClient, Options: query.DefaultOptions()} // TODO: what's the impact of DefaultOptions()?
 	resp, err := query.Staking_Params()
 	if err != nil {
-		return stakingtypes.Params{}, err
+		return &stakingtypes.Params{}, err
 	}
 
-	return resp.Params, nil
+	return &resp.Params, nil
 }
 
 // QueryEpochingParams queries epoching module's parameters via ChainClient
 // code is adapted from https://github.com/strangelove-ventures/lens/blob/v0.5.1/client/query/staking.go#L7-L18
-func (c *Client) QueryEpochingParams() (epochingtypes.Params, error) {
-	query := query.Query{Client: c.ChainClient, Options: query.DefaultOptions()} // TODO: what's the impact of DefaultOptions()?
+func (c *Client) QueryEpochingParams() (*epochingtypes.Params, error) {
+	query := query.Query{Client: c.ChainClient, Options: query.DefaultOptions()}
 	ctx, cancel := query.GetQueryContext()
 	defer cancel()
 
@@ -29,9 +31,25 @@ func (c *Client) QueryEpochingParams() (epochingtypes.Params, error) {
 	req := &epochingtypes.QueryParamsRequest{}
 	resp, err := queryClient.Params(ctx, req)
 	if err != nil {
-		return epochingtypes.Params{}, err
+		return &epochingtypes.Params{}, err
 	}
-	return resp.Params, nil
+	return &resp.Params, nil
+}
+
+// QueryHeaderChainTip queries hash/height of the latest BTC block in the btclightclient module
+func (c *Client) QueryHeaderChainTip() (*chainhash.Hash, uint64, error) {
+	query := query.Query{Client: c.ChainClient, Options: query.DefaultOptions()}
+	ctx, cancel := query.GetQueryContext()
+	defer cancel()
+
+	queryClient := btclightclienttypes.NewQueryClient(c.ChainClient)
+	req := &btclightclienttypes.QueryTipRequest{}
+	resp, err := queryClient.Tip(ctx, req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return resp.Header.Hash.ToChainhash(), resp.Header.Height, nil
 }
 
 // TODO: implement necessary queries here
--- reporter/bootstrapping.go
+++ reporter/bootstrapping.go
@@ -1,6 +1,8 @@
 package reporter
 
 import (
+	"time"
+
 	"github.com/babylonchain/vigilante/types"
 	"github.com/btcsuite/btcd/btcjson"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
@@ -8,10 +10,63 @@ import (
 )
 
 func (r *Reporter) Init() {
+	var (
+		btcLatestBlockHash   *chainhash.Hash
+		btcLatestBlockHeight int32
+		bbnLatestBlockHash   *chainhash.Hash
+		bbnLatestBlockHeight uint64
+		err                  error
+	)
+
+	// TODO: retrieve k and w
+
+	// retrieve hash/height of the latest block in BTC
+	btcLatestBlockHash, btcLatestBlockHeight, err = r.btcClient.GetBestBlock()
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("BTC latest block hash and height: (%v, %d)", btcLatestBlockHash, btcLatestBlockHeight)
+
+	// retrieve hash/height of the latest block in BBN header chain
+	bbnLatestBlockHash, bbnLatestBlockHeight, err = r.babylonClient.QueryHeaderChainTip()
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("BBN header chain latest block hash and height: (%v, %d)", bbnLatestBlockHash, bbnLatestBlockHeight)
+
+	// if BTC chain is shorter than BBN header chain, pause until BTC catches up
+	if uint64(btcLatestBlockHeight) < bbnLatestBlockHeight {
+		log.Infof("BTC chain (length %d) falls behind BBN header chain (length %d), wait until BTC catches up", btcLatestBlockHeight, bbnLatestBlockHeight)
+
+		// periodically check if BTC catches up with BBN.
+		// When BTC catches up, break and continue the bootstrapping process
+		ticker := time.NewTicker(10 * time.Second) // TODO: parameterise the polling interval
+		for range ticker.C {
+			btcLatestBlockHash, btcLatestBlockHeight, err = r.btcClient.GetBestBlock()
+			if err != nil {
+				panic(err)
+			}
+			bbnLatestBlockHash, bbnLatestBlockHeight, err = r.babylonClient.QueryHeaderChainTip()
+			if err != nil {
+				panic(err)
+			}
+			if uint64(btcLatestBlockHeight) >= bbnLatestBlockHeight {
+				log.Infof("BTC chain (length %d) now catches up with BBN header chain (length %d), continue bootstrapping", btcLatestBlockHeight, bbnLatestBlockHeight)
+				break
+			}
+		}
+	}
+
+	// TODO: initial consistency check
+
 	// Initialize BTC Cache
 	if err := r.initBTCCache(); err != nil {
 		panic(err)
 	}
+
+	// TODO: extract headers from BTC cache and forward them to BBN
+
+	// TODO: extract ckpt segments from BTC cache, store them in ckpt segment pool, check newly matched ckpts, and forward newly matched ckpts to BBN
 }
 
 // initBTCCache fetches the last blocks in the BTC canonical chain
