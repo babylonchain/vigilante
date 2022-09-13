3e80e758416e464a135198443d84f52b96214d4b
Runchao Han <elvisage941102@gmail.com>
2022-09-13 16:50:38 +1000

    temp fix

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -45,7 +45,7 @@ func New(cfg *config.ReporterConfig, btcClient *btcclient.Client, babylonClient 
 
 	k := btccParams.BtcConfirmationDepth
 	w := btccParams.CheckpointFinalizationTimeout
-	btcCacheMaxEntries := uint(k+w) + 1
+	btcCacheMaxEntries := uint(k+w) + 1000
 	// TODO: btcCacheMaxEntries can still be larger than k+w. We need to give a larger btc cache size here
 
 	log.Infof("BTCCheckpoint parameters: (k, w) = (%d, %d)", k, w)
