0bb16d363d070263ee0a607bade38114f3d46e4c
Runchao Han <elvisage941102@gmail.com>
2022-10-25 12:21:58 +1100

    minor

--- types/btctestdata/README.md
+++
@@ -1,4 +0,0 @@
-# btctestdata
-
-This directory contains test data related to BTC blocks. 
-The data is copied from https://github.com/btcsuite/btcd/tree/master/blockchain/testdata.
--- types/indexed_block_test.go
+++ types/indexed_block_test.go
@@ -5,118 +5,138 @@
 package types_test
 
 import (
-	"compress/bzip2"
-	"encoding/binary"
-	"io"
 	"math/rand"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/babylonchain/babylon/testutil/datagen"
 	btcctypes "github.com/babylonchain/babylon/x/btccheckpoint/types"
 	"github.com/babylonchain/vigilante/types"
+	"github.com/btcsuite/btcd/blockchain"
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	_ "github.com/btcsuite/btcd/database/ffldb"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
 	"github.com/stretchr/testify/require"
 )
 
-// loadBlocks reads files containing bitcoin block data (gzipped but otherwise
-// in the format bitcoind writes) from disk and returns them as an array of
-// btcutil.Block.  This is largely borrowed from the test code in btcdb.
-// copied from https://github.com/btcsuite/btcd/blob/master/blockchain/common_test.go
-func loadBlocks(filename string) (blocks []*btcutil.Block, err error) {
-	filename = filepath.Join("btctestdata/", filename)
+func newTestTx(babylonTx bool) *wire.MsgTx {
+	tx := &wire.MsgTx{
+		Version: 1,
+		TxIn: []*wire.TxIn{
+			{
+				PreviousOutPoint: wire.OutPoint{
+					Hash:  chainhash.Hash{},
+					Index: 0xffffffff,
+				},
+				SignatureScript: []byte{
+					0x04, 0x31, 0xdc, 0x00, 0x1b, 0x01, 0x62,
+				},
+				Sequence: 0xffffffff,
+			},
+		},
+		TxOut: []*wire.TxOut{
+			{
+				Value: 0x12a05f200,
+				PkScript: []byte{
+					0x41, // OP_DATA_65
+					0x04, 0xd6, 0x4b, 0xdf, 0xd0, 0x9e, 0xb1, 0xc5,
+					0xfe, 0x29, 0x5a, 0xbd, 0xeb, 0x1d, 0xca, 0x42,
+					0x81, 0xbe, 0x98, 0x8e, 0x2d, 0xa0, 0xb6, 0xc1,
+					0xc6, 0xa5, 0x9d, 0xc2, 0x26, 0xc2, 0x86, 0x24,
+					0xe1, 0x81, 0x75, 0xe8, 0x51, 0xc9, 0x6b, 0x97,
+					0x3d, 0x81, 0xb0, 0x1c, 0xc3, 0x1f, 0x04, 0x78,
+					0x34, 0xbc, 0x06, 0xd6, 0xd6, 0xed, 0xf6, 0x20,
+					0xd1, 0x84, 0x24, 0x1a, 0x6a, 0xed, 0x8b, 0x63,
+					0xa6, // 65-byte signature
+					0xac, // OP_CHECKSIG
+				},
+			},
+			{
+				Value: 0x5f5e100,
+				PkScript: []byte{
+					0x41, // OP_DATA_65
+					0x04, 0xd6, 0x4b, 0xdf, 0xd0, 0x9e, 0xb1, 0xc5,
+					0xfe, 0x29, 0x5a, 0xbd, 0xeb, 0x1d, 0xca, 0x42,
+					0x81, 0xbe, 0x98, 0x8e, 0x2d, 0xa0, 0xb6, 0xc1,
+					0xc6, 0xa5, 0x9d, 0xc2, 0x26, 0xc2, 0x86, 0x24,
+					0xe1, 0x81, 0x75, 0xe8, 0x51, 0xc9, 0x6b, 0x97,
+					0x3d, 0x81, 0xb0, 0x1c, 0xc3, 0x1f, 0x04, 0x78,
+					0x34, 0xbc, 0x06, 0xd6, 0xd6, 0xed, 0xf6, 0x20,
+					0xd1, 0x84, 0x24, 0x1a, 0x6a, 0xed, 0x8b, 0x63,
+					0xa6, // 65-byte signature
+					0xac, // OP_CHECKSIG
+				},
+			},
+		},
+		LockTime: 0,
+	}
+	if babylonTx {
+		tx.TxOut[0].PkScript = []byte{} // TODO: babylon
+	}
+	return tx
+}
 
-	var network = wire.MainNet
-	var dr io.Reader
-	var fi io.ReadCloser
-
-	fi, err = os.Open(filename)
-	if err != nil {
-		return
+// calcMerkleRoot creates a merkle tree from the slice of transactions and
+// returns the root of the tree.
+func calcMerkleRoot(txns []*wire.MsgTx) chainhash.Hash {
+	if len(txns) == 0 {
+		return chainhash.Hash{}
 	}
 
-	if strings.HasSuffix(filename, ".bz2") {
-		dr = bzip2.NewReader(fi)
-	} else {
-		dr = fi
+	utilTxns := make([]*btcutil.Tx, 0, len(txns))
+	for _, tx := range txns {
+		utilTxns = append(utilTxns, btcutil.NewTx(tx))
 	}
-	defer fi.Close()
+	merkles := blockchain.BuildMerkleTreeStore(utilTxns, false)
+	return *merkles[len(merkles)-1]
+}
 
-	var block *btcutil.Block
-
-	err = nil
-	for height := int64(1); err == nil; height++ {
-		var rintbuf uint32
-		err = binary.Read(dr, binary.LittleEndian, &rintbuf)
-		if err == io.EOF {
-			// hit end of file at expected offset: no warning
-			height--
-			err = nil
-			break
-		}
-		if err != nil {
-			break
-		}
-		if rintbuf != uint32(network) {
-			break
-		}
-		err = binary.Read(dr, binary.LittleEndian, &rintbuf)
-		blocklen := rintbuf
-
-		rbytes := make([]byte, blocklen)
-
-		// read block
-		dr.Read(rbytes)
-
-		block, err = btcutil.NewBlockFromBytes(rbytes)
-		if err != nil {
-			return
-		}
-		blocks = append(blocks, block)
+func genRandomBlocksWithBabylonTx(n int, percentage float32) ([]*types.IndexedBlock, []bool) {
+	blocks := []*types.IndexedBlock{}
+	isBabylonBlockArray := []bool{}
+	// percentage should be [0, 1]
+	if percentage < 0 || percentage > 1 {
+		return blocks, isBabylonBlockArray
 	}
 
-	return
+	for i := 0; i < n; i++ {
+		// create a tx, which will be a Babylon tx with probability `percentage`
+		coinbaseTx := newTestTx(false)
+		var msgTx *wire.MsgTx
+		if rand.Float32() < percentage {
+			msgTx = newTestTx(true)
+			isBabylonBlockArray = append(isBabylonBlockArray, true)
+		} else {
+			msgTx = newTestTx(false)
+			isBabylonBlockArray = append(isBabylonBlockArray, false)
+		}
+		msgTxs := []*wire.MsgTx{coinbaseTx, msgTx}
+		merkleRoot := calcMerkleRoot(msgTxs)
+
+		// construct and append block
+		header := datagen.GenRandomBtcdHeader()
+		header.MerkleRoot = merkleRoot
+		ib := types.NewIndexedBlock(rand.Int31(), header, []*btcutil.Tx{btcutil.NewTx(coinbaseTx), btcutil.NewTx(msgTx)})
+		blocks = append(blocks, ib)
+	}
+	return blocks, isBabylonBlockArray
 }
 
 func FuzzIndexedBlock(f *testing.F) {
-	testFiles := []string{
-		"blk_0_to_4.dat.bz2",
-		"blk_3A.dat.bz2",
-	}
-
-	// get a list of IndexedBlock from testdata
-	var blocks []*types.IndexedBlock
-	for _, file := range testFiles {
-		tmpBlocks, err := loadBlocks(file)
-		if err != nil {
-			f.Errorf("Error loading file: %v\n", err)
-			return
-		}
-		for _, tmpBlock := range tmpBlocks {
-			height := tmpBlock.Height()
-			header := &tmpBlock.MsgBlock().Header
-			txs := types.GetWrappedTxs(tmpBlock.MsgBlock())
-			indexedBlock := types.NewIndexedBlock(height, header, txs)
-			blocks = append(blocks, indexedBlock)
-		}
-	}
-
 	datagen.AddRandomSeedsToFuzzer(f, 10)
 
 	f.Fuzz(func(t *testing.T, seed int64) {
 		rand.Seed(seed)
 
-		for _, block := range blocks {
+		blocks, isBabylonBlockArray := genRandomBlocksWithBabylonTx(100, 0.4)
+		for i, block := range blocks {
 			// skip blocks with no tx
 			if len(block.Txs) == 0 {
 				continue
 			}
-			idx := rand.Int() % len(block.Txs)
+			idx := rand.Intn(len(block.Txs))
 
 			spvProof, err := block.GenSPVProof(idx)
 			require.NoError(t, err)
@@ -129,12 +149,15 @@ func FuzzIndexedBlock(f *testing.F) {
 				chaincfg.SimNetParams.PowLimit,
 			)
 
-			// ParseProof requires the tx to have OP_RETURN data, which is out of the scope of verifying Merkle proofs
-			if strings.Contains(err.Error(), "provided transaction should provide op return data") {
-				continue
+			if isBabylonBlockArray[i] {
+				require.NotNil(t, parsedProof)
+				require.NoError(t, err)
+			} else {
+				// ParseProof requires the tx to have OP_RETURN data, which is out of the scope of verifying Merkle proofs
+				if strings.Contains(err.Error(), "provided transaction should provide op return data") {
+					continue
+				}
 			}
-			require.NotNil(t, parsedProof)
-			require.NoError(t, err)
 		}
 	})
 }
