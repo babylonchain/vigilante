0650e6b353fe478da96eb15d93f1e6ec72275935
Runchao Han <elvisage941102@gmail.com>
2022-10-28 16:13:48 +1100

    fix ci

--- reporter/utils_test.go
+++ reporter/utils_test.go
@@ -16,15 +16,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func newMockReporter(t *testing.T) (*mocks.MockBTCClient, *mocks.MockBabylonClient, *reporter.Reporter) {
+func newMockReporter(t *testing.T, ctrl *gomock.Controller) (*mocks.MockBTCClient, *mocks.MockBabylonClient, *reporter.Reporter) {
 	cfg := config.DefaultConfig()
 
-	mockBTCClient := mocks.NewMockBTCClient(gomock.NewController(t))
-
-	mockBabylonClient := mocks.NewMockBabylonClient(gomock.NewController(t))
+	mockBTCClient := mocks.NewMockBTCClient(ctrl)
+	mockBabylonClient := mocks.NewMockBabylonClient(ctrl)
 	btccParams := btcctypes.DefaultParams()
-	mockBabylonClient.EXPECT().MustQueryBTCCheckpointParams().Return(&btccParams)
-	mockBabylonClient.EXPECT().GetTagIdx().Return(uint8(48))
+	mockBabylonClient.EXPECT().MustQueryBTCCheckpointParams().Return(&btccParams).AnyTimes()
+	mockBabylonClient.EXPECT().GetTagIdx().Return(uint8(48)).AnyTimes()
+	mockBabylonClient.EXPECT().GetConfig().Return(&cfg.Babylon).AnyTimes()
 
 	reporter, err := reporter.New(
 		&cfg.Reporter,
@@ -38,17 +38,15 @@ func newMockReporter(t *testing.T) (*mocks.MockBTCClient, *mocks.MockBabylonClie
 	return mockBTCClient, mockBabylonClient, reporter
 }
 
-// TODO: tests on handler functions
-// - given a BTC block with/without OP_RETURN txs/signer, test parsing/extracting ckpts in this block
-// - given a BTC block, test parsing/extracting header in this block
-
 // FuzzProcessHeaders is a fuzz tests for ProcessHeaders()
 // - Data: a number of random blocks, with or without Babylon txs
 // - Tested property: for any BTC block, if its header is not duplicated, then it will submit this header
 func FuzzProcessHeaders(f *testing.F) {
-	datagen.AddRandomSeedsToFuzzer(f, 100)
+	datagen.AddRandomSeedsToFuzzer(f, 10)
 
 	f.Fuzz(func(t *testing.T, seed int64) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
 		rand.Seed(seed)
 
 		containsBlock := datagen.OneInN(2)
@@ -56,17 +54,22 @@ func FuzzProcessHeaders(f *testing.F) {
 		block, _ := vdatagen.GenRandomBlock(false, nil)
 		ib := types.NewIndexedBlockFromMsgBlock(rand.Int31(), block)
 
-		_, mockBabylonClient, reporter := newMockReporter(t)
+		_, mockBabylonClient, reporter := newMockReporter(t, ctrl)
 
-		mockBabylonClient.EXPECT().QueryContainsBlock(gomock.Any()).Return(containsBlock, nil)
-		mockBabylonClient.EXPECT().InsertHeaders(gomock.Any()).Return(&sdk.TxResponse{Code: 0}, nil)
+		// may or may not contain this block
+		mockBabylonClient.EXPECT().QueryContainsBlock(gomock.Any()).Return(containsBlock, nil).AnyTimes()
+		// inserting header will always be successful
+		mockBabylonClient.EXPECT().InsertHeaders(gomock.Any()).Return(&sdk.TxResponse{Code: 0}, nil).AnyTimes()
 
 		// if Babylon client contains this block, numSubmitted has to be 0, otherwise 1
 		numSubmitted := reporter.ProcessHeaders(nil, []*types.IndexedBlock{ib})
 		if containsBlock {
-			require.Equal(t, 1, numSubmitted)
-		} else {
 			require.Equal(t, 0, numSubmitted)
+		} else {
+			require.Equal(t, 1, numSubmitted)
 		}
+
 	})
 }
+
+// TODO: given a BTC block with/without OP_RETURN txs/signer, test parsing/extracting ckpts in this block
