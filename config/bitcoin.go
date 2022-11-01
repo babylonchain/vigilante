9f91d8ed17bd68969c3f8bd736f214dd3f182791
Gurjot <gurjotsyd@gmail.com>
2022-11-01 19:38:47 -0400

    remove buffer from config

--- config/bitcoin.go
+++ config/bitcoin.go
@@ -9,22 +9,21 @@ import (
 
 // BTCConfig defines configuration for the Bitcoin client
 type BTCConfig struct {
-	DisableClientTLS     bool                            `mapstructure:"no-client-tls"`
-	CAFile               string                          `mapstructure:"ca-file"`
-	Endpoint             string                          `mapstructure:"endpoint"`
-	WalletEndpoint       string                          `mapstructure:"wallet-endpoint"`
-	WalletPassword       string                          `mapstructure:"wallet-password"`
-	WalletName           string                          `mapstructure:"wallet-name"`
-	WalletCAFile         string                          `mapstructure:"wallet-ca-file"`
-	WalletLockTime       int64                           `mapstructure:"wallet-lock-time"` // time duration in which the wallet remains unlocked, in seconds
-	TxFee                btcutil.Amount                  `mapstructure:"tx-fee"`           // BTC tx fee, in BTC
-	NetParams            string                          `mapstructure:"net-params"`
-	Username             string                          `mapstructure:"username"`
-	Password             string                          `mapstructure:"password"`
-	ReconnectAttempts    int                             `mapstructure:"reconnect-attempts"`
-	SubscriptionMode     types.SupportedSubscriptionMode `mapstructure:"subscription-mode"`
-	ZmqEndpoint          string                          `mapstructure:"zmq-endpoint"`
-	ZmqChannelBufferSize int                             `mapstructure:"zmq-channel-buffer-size"`
+	DisableClientTLS  bool                            `mapstructure:"no-client-tls"`
+	CAFile            string                          `mapstructure:"ca-file"`
+	Endpoint          string                          `mapstructure:"endpoint"`
+	WalletEndpoint    string                          `mapstructure:"wallet-endpoint"`
+	WalletPassword    string                          `mapstructure:"wallet-password"`
+	WalletName        string                          `mapstructure:"wallet-name"`
+	WalletCAFile      string                          `mapstructure:"wallet-ca-file"`
+	WalletLockTime    int64                           `mapstructure:"wallet-lock-time"` // time duration in which the wallet remains unlocked, in seconds
+	TxFee             btcutil.Amount                  `mapstructure:"tx-fee"`           // BTC tx fee, in BTC
+	NetParams         string                          `mapstructure:"net-params"`
+	Username          string                          `mapstructure:"username"`
+	Password          string                          `mapstructure:"password"`
+	ReconnectAttempts int                             `mapstructure:"reconnect-attempts"`
+	SubscriptionMode  types.SupportedSubscriptionMode `mapstructure:"subscription-mode"`
+	ZmqEndpoint       string                          `mapstructure:"zmq-endpoint"`
 }
 
 func (cfg *BTCConfig) Validate() error {
@@ -44,10 +43,6 @@ func (cfg *BTCConfig) Validate() error {
 		// TODO: implement regex validation for zmq endpoint
 		if cfg.ZmqEndpoint == "" {
 			return errors.New("zmq endpoint cannot be empty")
-		}
-
-		if cfg.ZmqChannelBufferSize <= 0 {
-			return errors.New("ZMQ channel buffer size must be positive")
 		}
 	}
 
