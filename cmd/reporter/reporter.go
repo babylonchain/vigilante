a194f03a9662289acdec62ccbd44191c4b7f9155
Runchao Han <elvisage941102@gmail.com>
2023-02-20 17:00:28 +1100

    f

--- cmd/reporter/reporter.go
+++ cmd/reporter/reporter.go
@@ -2,7 +2,6 @@ package reporter
 
 import (
 	"fmt"
-	"net/http"
 	_ "net/http/pprof"
 
 	bbnclient "github.com/babylonchain/rpc-client/client"
@@ -93,10 +92,6 @@ func cmdFunc(cmd *cobra.Command, args []string) {
 	// start Prometheus metrics server
 	addr := fmt.Sprintf("%s:%d", cfg.Metrics.Host, cfg.Metrics.ServerPort)
 	metrics.Start(addr)
-
-	go func() {
-		log.Println(http.ListenAndServe("localhost:10000", nil))
-	}()
 
 	// SIGINT handling stuff
 	utils.AddInterruptHandler(func() {
