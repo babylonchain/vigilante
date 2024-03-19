02970001350ed98a565a57dea979b7a7de430b06
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-03-19 11:29:51 -0300

    chore: removed unused code

--- btcstaking-tracker/btcslasher/slasher_utils.go
+++ btcstaking-tracker/btcslasher/slasher_utils.go
@@ -183,22 +183,6 @@ func (bs *BTCSlasher) sendSlashingTx(
 	return txHash, nil
 }
 
-// findFPIdxInWitness returns the index of the finality provider's signature
-// in the witness stack of 1-out-of-n multisig from finality providers
-// Note: the signatures are sorted in reverse lexical order since the PKs
-// in the staking script are sorted in lexical order and BTC script execution
-// is stack based
-// func findFPIdxInWitness(fpSK *btcec.PrivateKey, fpBTCPKs []bbn.BIP340PubKey) (int, error) {
-// 	fpBTCPK := bbn.NewBIP340PubKeyFromBTCPK(fpSK.PubKey())
-// 	sortedFPBTCPKList := bbn.SortBIP340PKs(fpBTCPKs)
-// 	for i, pk := range sortedFPBTCPKList {
-// 		if pk.Equals(fpBTCPK) {
-// 			return i, nil
-// 		}
-// 	}
-// 	return 0, fmt.Errorf("the given finality provider's PK is not found in the BTC delegation")
-// }
-
 // BuildUnbondingSlashingTxWithWitness returns the unbonding slashing tx.
 func BuildUnbondingSlashingTxWithWitness(
 	d *bstypes.BTCDelegationResponse,
